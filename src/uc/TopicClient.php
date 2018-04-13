c7300438625109481464406d45eb023b50f9dcb2
cc <chenc@kuai8.com>
2018-04-13 16:42:03 +0800

    动态

--- src/uc/TopicClient.php
+++ src/uc/TopicClient.php
@@ -21,33 +21,83 @@ class TopicClient extends CurlAbstract {
     }
     
     //动态展示
-    public function topicList() {
-        
-    }
-    
-    //动态详情（评论列表）
-    public function topicInfo() {
-        
+    public function topicList($limit,$page) {
+        $url = $this->uri.strtolower(__FUNCTION__);
+        $options = [
+            'limit'=>$limit,'page'=>$page
+        ];
+        $this->params = array_merge($this->params,$options);
+        return $this->get($url,$this->params);
     }
     
     //动态发布
-    public function topicRelease() {
-        
+    public function topicRelease($imgs,$content,$uid) {
+        $url = $this->uri.strtolower(__FUNCTION__);
+        $options = [
+            'imgs'=>$imgs,'content'=>$content,'uid'=>$uid
+        ];
+        $this->params = array_merge($this->params,$options);
+        return $this->get($url,$this->params);
     }
     
-    //关注、删除关注用户
-    public function userAttention() {
-        
-    }
-    
-    //评论发布
-    public function commentRelease() {
-        
+    //关注、取消关注用户（非本人发布的）
+    public function userAttention($follow_id,$type,$follow_uid,$uid) {
+        $url = $this->uri.strtolower(__FUNCTION__);
+        $options = [
+            'follow_id'=>$follow_id,'type'=>$type,'follow_uid'=>$follow_uid,'uid'=>$uid
+        ];
+        $this->params = array_merge($this->params,$options);
+        return $this->get($url,$this->params);
     }
     
     //点赞、踩动态
-    public function topicAgree() {
-        
+    public function topicAgree($agree_id,$type,$topic_id,$uid) {
+        $url = $this->uri.strtolower(__FUNCTION__);
+        $options = [
+            'agree_id'=>$agree_id,'type'=>$type,'topic_id'=>$topic_id,'uid'=>$uid
+        ];
+        $this->params = array_merge($this->params,$options);
+        return $this->get($url,$this->params);
+    }
+    
+    //举报动态
+    public function topicBad($bad_content,$topic_id,$uid) {
+        $url = $this->uri.strtolower(__FUNCTION__);
+        $options = [
+            'bad_content'=>$bad_content,'topic_id'=>$topic_id,'uid'=>$uid
+        ];
+        $this->params = array_merge($this->params,$options);
+        return $this->get($url,$this->params);
+    }
+    
+    //分享动态
+    public function topicShare($share_target,$topic_id,$uid) {
+        $url = $this->uri.strtolower(__FUNCTION__);
+        $options = [
+            'share_target'=>$share_target,'topic_id'=>$topic_id,'uid'=>$uid
+        ];
+        $this->params = array_merge($this->params,$options);
+        return $this->get($url,$this->params);
+    }
+    
+    //动态详情（评论列表）
+    public function topicInfo($topic_id) {
+        $url = $this->uri.strtolower(__FUNCTION__);
+        $options = [
+            'topic_id'=>$topic_id
+        ];
+        $this->params = array_merge($this->params,$options);
+        return $this->get($url,$this->params);
+    }
+    
+    //评论发布
+    public function commentRelease($topic_id,$reply_content,$uid) {
+        $url = $this->uri.strtolower(__FUNCTION__);
+        $options = [
+            'reply_content'=>$reply_content,'topic_id'=>$topic_id,'uid'=>$uid
+        ];
+        $this->params = array_merge($this->params,$options);
+        return $this->get($url,$this->params);
     }
     
 }
